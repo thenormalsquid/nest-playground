cf2467ccf8c293b301e8dead74cdc6a2725a479a
tman <tbhuynh10@gmail.com>
2021-02-05 00:08:48 -0800

    configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SakeModule } from './sake/sake.module';
 
 @Module({
-  imports: [SakeModule],
+  imports: [SakeModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 6697,
+    username: 'postgres',
+    password: 'pass123',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
