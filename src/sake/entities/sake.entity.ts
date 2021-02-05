2bbf1350bd0186586eba6825677819028d61eba5
tman <tbhuynh10@gmail.com>
2021-02-05 00:15:42 -0800

    make sake entity typeorm entity

--- src/sake/entities/sake.entity.ts
+++ src/sake/entities/sake.entity.ts
@@ -1,11 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export type Category = 'daiginjo' | 'ginjo' | 'nigori' | 'junmai' | 'honjozo';
 
+@Entity()
 export class Sake {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   category: Category;
+
+  @Column()
   company: string;
+
+  @Column()
   alcohol: string;
+
+  @Column()
   region: string;
+
+  @Column('json', { nullable: true })
   flavors?: string[];
 }
--- src/sake/sake.module.ts
+++ src/sake/sake.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SakeController } from './sake.controller';
 import { SakeService } from './sake.service';
+import { Sake } from './entities/sake.entity';
 
-@Module({ controllers: [SakeController], providers: [SakeService] })
+@Module({ imports: [TypeOrmModule.forFeature([Sake])], controllers: [SakeController], providers: [SakeService] })
 export class SakeModule {}
