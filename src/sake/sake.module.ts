a51c5be10fcd9dcc50a641238e89e1ca9cf8d94f
tman <tbhuynh10@gmail.com>
2021-02-16 11:52:49 -0800

    add useFactory for dynamically creating a brand provider

--- src/sake/sake.module.ts
+++ src/sake/sake.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SakeController } from './sake.controller';
 import { SakeService } from './sake.service';
@@ -10,22 +10,33 @@ import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 
 export class MockSakeService {}
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+// class ConfigService {}
+// class DevelopmentConfigService {}
+// class ProductionConfigService {}
+@Injectable()
+class SakeBrandsFactory {
+  create() {
+    return ['foo', 'bar'];
+  }
+}
 @Module({
   imports: [TypeOrmModule.forFeature([Sake, Flavor, Company, Event])],
   controllers: [SakeController],
   providers: [
     SakeService,
+    SakeBrandsFactory,
+    // {
+    //   provide: ConfigService,
+    //   useClass:
+    //     process.env.NODE_ENV === 'development'
+    //       ? DevelopmentConfigService
+    //       : ProductionConfigService,
+    // },
     {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
+      provide: SAKE_BRANDS,
+      useFactory: (brandsFactory: SakeBrandsFactory) => brandsFactory.create(),
+      inject: [SakeBrandsFactory],
     },
-    { provide: SAKE_BRANDS, useValue: ['foo', 'bar'] },
   ],
   exports: [SakeService],
 })
